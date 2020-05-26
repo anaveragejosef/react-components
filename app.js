bb3a32029cc6a446dab2a9e442fec5337a42e188
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-26 12:02:26 -0700

    Completes the Solo Sprint
    - This commit contains the stateless functional component for the
    GroceryList and the class component GroceryListItem.
    - It also allows for the grocery items to be passed in and the list to
    populate dynamically using .map.
    - Lastly it adds an onClick, onMouseOver, and onMouseOut functions to
    either strikethrough or bold the text.

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+    {props.foods.map(food =>
+      <GroceryListItem food={food} />
+    )}
+  </ul>
+);
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+      hover: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onItemHover() {
+    this.setState({
+      hover: true
+    });
+  }
+
+  onItemExit() {
+    this.setState({
+      hover: false
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)} onMouseOver={this.onItemHover.bind(this)} onMouseOut={this.onItemExit.bind(this)}>{this.props.food}</li>
+    );
+  }
+}
+
+ReactDOM.render(<GroceryList foods={['Waffles', 'Maple Syrup']}/>, document.getElementById("app"));
+
+
+// var Waffles = () => (
+//   <li>Waffles</li>
+// );
+
+// var MapleSyrup = () => (
+//   <li>Maple Syrup</li>
+// );
